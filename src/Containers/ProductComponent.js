5abfb1ea5e45aaa0a051bcf669e7cff83da6a625
mayuripy <115979827+mayuripy@users.noreply.github.com>
2022-12-16 17:58:20 +0530

    ,,,,,

--- src/Containers/ProductComponent.js
+++ src/Containers/ProductComponent.js
@@ -1,26 +1,39 @@
 import React from "react";
+import {Link} from "react-router-dom";
 import { useSelector } from "react-redux";
 
 
 const ProductComponent = () => {
   const products= useSelector((state) => state);
   console.log(products)
-  //const {id,title} = products[0];
+  const renderlist = products.map((products) => {
+  const {id,title,image,price,category} = products;
+
   return (
-  <div className="four columns wide">
+
+    <div className="four wide columns" key={id} >
+      <Link to={'/products/ ${id}'}>
     <div className="ui link card">
       <div className="card">
         <div className="image">
-          <div className="content">
-            {/* <div className="header">{title}</div> */}
+          <img src={image} alt={title}/>
 
+          <div className="content">
+             <div className="header">{title}</div>
+              <div className="meta price">$ {price}</div>
+              <div className="price">{category}</div>
             
           </div>
         </div>
       </div>
     </div>
+    </Link>
     </div>
-    )
+    
+    
+    );
+      
+});
+ return <>{renderlist}</>;
 };
-
 export default ProductComponent;
